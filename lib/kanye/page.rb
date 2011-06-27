6d2474688a67c05dd1225a47a1a0de2fe4cdfea9
anark <anark@comcast.net>
2011-06-26 19:42:57 -0700

    use kanye db_path for db

--- lib/kanye/page.rb
+++ lib/kanye/page.rb
@@ -14,7 +14,7 @@ module Kanye
     def download_all!
       while tracks.size > 0
         current_track = tracks.pop
-        history = History.new(db_path)
+        history = History.new(Kanye.db_path)
         unless history.exists?(current_track)
           begin
             current_track.download! 
